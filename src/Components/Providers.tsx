59e75e9c8c336bde1353ffff2d4c5050c79b2a67
jordan williamson <williamsonjordan05@gmail.com>
2024-08-12 19:53:35 -0600

    initial commit

--- src/Components/Providers.tsx
+++ src/Components/Providers.tsx
@@ -3,6 +3,11 @@ import { Provider } from '../Utility/Types'
 
 const Providers: React.FC = () => {
     const [providers, setProviders] = useState<Provider[]>([])
+    useEffect(() => {
+        fetch('http://localhost:3001/providers')
+            .then(response => response.json())
+            .then(data => setProviders(data))
+    }, [])
   return (
     <div>Providers</div>
   )
@@ -11,11 +16,11 @@ const Providers: React.FC = () => {
 export default Providers
 
 const providers = [
-    
+    {
+        name: 'Kids On The Move',
+        website: 'kotm.org',
+        phone: '(385) 292-5633',
+        email: '',
+        address: '475 West 260 North Orem, UT 84057'
+    }
 ]
-
-Kids On The Move
-Website: kotm.org
-Phone: (385) 292-5633
-E-mail: autismintake@kotm.org
-Address: 475 West 260 North Orem, UT 84057
